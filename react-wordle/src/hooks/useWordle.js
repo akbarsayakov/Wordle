47343d9037efa31eadaf0d6307a163a017d2cb13
unknown <akbarsayakov@gmail.com>
2022-12-08 00:47:32 -0800

    Made and styled the keypad

--- react-wordle/src/hooks/useWordle.js
+++ react-wordle/src/hooks/useWordle.js
@@ -6,6 +6,7 @@ const useWordle = (solution) => {
   const [guesses, setGuesses] = useState([...Array(6)]) // array format of guesses
   const [history, setHistory] = useState([]) // string format of guesses
   const [isCorrect, setIsCorrect] = useState(false)
+  const [usedKeys, setUsedKeys] = useState({}) // {a: 'green', b: 'yellow', c: 'grey'}
 
   // format a guess into an array of letter objects
   // e.g [{key: 'a', color: 'yellow'}]
@@ -51,6 +52,30 @@ const useWordle = (solution) => {
     setTurn((prevTurn) => {
       return prevTurn + 1
     })
+    setUsedKeys((prevUsedKeys) => {
+      let newKeys = { ...prevUsedKeys }
+      formattedGuess.forEach((l) => {
+        const currentColor = newKeys[l.key]
+
+        if (l.color === 'green') {
+          newKeys[l.key] = 'green'
+          return
+        }
+        if (l.color === 'yellow' && currentColor !== 'green') {
+          newKeys[l.key] = 'yellow'
+          return
+        }
+        if (
+          l.color === 'grey' &&
+          currentColor !== 'green' &&
+          currentColor !== 'yellow'
+        ) {
+          newKeys[l.key] = 'grey'
+          return
+        }
+      })
+      return newKeys
+    })
     setCurrentGuess('')
   }
 
@@ -92,7 +117,7 @@ const useWordle = (solution) => {
     }
   }
 
-  return { turn, currentGuess, guesses, isCorrect, handleKeyup }
+  return { turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyup }
 }
 
 export default useWordle
